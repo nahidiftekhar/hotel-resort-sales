10ece5527a939853251f216e48e72b85376ee6ba
Nahid Iftekhar <nahid.iftekhar13@gmail.com>
2023-04-10 11:35:48 +0600

    Standard config for FE

--- frontend/configs/backend.js
+++ frontend/configs/backend.js
@@ -1,8 +1,5 @@
 const beConfig = {
-    host: "http://localhost:3420/",
-    // host: "https://link.smarttaps.co/",
-    // host: "27.147.174.60:3420",
-    port: 5432,
+    host: "http://localhost:3426/",
 }
 
 module.exports = {
