891a9ff3e89315bf8affcbaad65dac18b0f3fea0
Nahid Iftekhar <100844948+nahidiftekhar@users.noreply.github.com>
2023-04-10 12:11:45 +0600

    Standard configs

---
+++ backend/configs/config.js
@@ -0,0 +1,7 @@
+const config = {
+    PORT: 3426,
+    listPerPage: 10,
+    WHITELISTED_DOMAINS:  ["http://localhost:3425", "http://localhost", "http://localhost:3000"],
+    APP_URL: "localhost:3425"
+};
+module.exports = {config};
