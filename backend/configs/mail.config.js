32ef530cd74e1fad1b8edbcb0440306ff13a00f4
Nahid Iftekhar <nahid.iftekhar13@gmail.com>
2023-04-10 12:49:39 +0600

    Standard configs

--- backend/configs/mail.config.js
+++ backend/configs/mail.config.js
@@ -1,28 +1,12 @@
-<<<<<<< HEAD
 const mailConfig = {
-    EMAIL_USERNAME: 'FULL-EMAIL-ID', //info@codemarshal.com
-    EMAIL_PASSWORD: 'EMAIL-PASSOWRD',
-    EMAIL_PORT: '465', //may depend on mail server
-    EMAIL_HOST: 'MAIL-HOST-MNAME',//'server107.web-hosting.com',
-    // SERVER_NAME: 'www.smarttaps.co', --required for bluehost. may be some other hosts
-    EMAIL_TO: 'FULL-EMAIL-ID',// for default receipient if any. 'nahid.iftekhar@codemarshal.com',
-    DEFAULT_TEXT: 'This mail is generated from backend with nodemailer'
+    EMAIL_USERNAME: 'jogfol@bdlancelimited.com',
+    EMAIL_PASSWORD: 'rjD9%iSH#fRL',
+    EMAIL_PORT: '465',
+    EMAIL_HOST: 'server107.web-hosting.com',
+    EMAIL_TO: 'nahid.iftekhar@codemarshal.com',
+    DEFAULT_TEXT: 'This mail is generated from appointment service'
     }
 
 module.exports = {
     mailConfig
-=======
-const mailConfig = {
-    EMAIL_USERNAME: 'FULL-EMAIL-ID', //info@codemarshal.com
-    EMAIL_PASSWORD: 'EMAIL-PASSOWRD',
-    EMAIL_PORT: '465', //may depend on mail server
-    EMAIL_HOST: 'MAIL-HOST-MNAME',//'server107.web-hosting.com',
-    // SERVER_NAME: 'www.smarttaps.co', --required for bluehost. may be some other hosts
-    EMAIL_TO: 'FULL-EMAIL-ID',// for default receipient if any 'nahid.iftekhar@codemarshal.com',
-    DEFAULT_TEXT: 'This mail is generated from backend with nodemailer'
-    }
-
-module.exports = {
-    mailConfig
->>>>>>> 011af36ede3115be1e8b55752f1bd5bca50d3e26
 }
--- backend/configs/secrets.config.js
+++ backend/configs/secrets.config.js
@@ -1,6 +1,8 @@
-<<<<<<< HEAD
 const secretConfig = {
   SALT_ROUNDS: 10,
+  JWT_SECRET: "jdhdhd-kjfjdhrhrerj-uurhr-jjge",
+  REFRESH_TOKEN_SECRET: "fgkjddshfdjh773bdjsj84-jdjd774",
+  SESSION_COOKIE_SECRET: ",sttujsfs;oyy;rjanc391iikwppbdki",
   SESSION_COOKIE_NAME: "NIE_Cookie",
   ACCESS_TOKEN_EXPIRY: 43200000,
   REFRESH_TOKEN_EXPIRY: 864000000,
@@ -8,14 +10,3 @@ const secretConfig = {
   REFRESH_TOKEN: "CMRT"
 };
 module.exports = { secretConfig };
-=======
-const secretConfig = {
-  SALT_ROUNDS: 10,
-  SESSION_COOKIE_NAME: "NIE_Cookie",
-  ACCESS_TOKEN_EXPIRY: 43200000,
-  REFRESH_TOKEN_EXPIRY: 864000000,
-  ACCESS_TOKEN: "CMAT",
-  REFRESH_TOKEN: "CMRT"
-};
-module.exports = { secretConfig };
->>>>>>> 011af36ede3115be1e8b55752f1bd5bca50d3e26
